753f7645f752144aa567870bde268b53b411d07e
Damilare Emmanuel <emmanuel@platformlead.com>
2019-06-06 15:56:06 +0100

    implemented google auth

---
+++ server/client/src/components/Landing.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>EmailCampaigner</h1>
+      Get users feedback to help your business
+    </div>
+  );
+};
+export default Landing;
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -8,18 +8,24 @@ module.exports = app => {
     })
   );
 
-  app.get("/auth/google/callback", passport.authenticate("google"));
+  app.get(
+    "/auth/google/callback",
+    passport.authenticate("google"),
+    (req, res) => {
+      res.redirect("/surveys");
+    }
+  );
 
   app.get("/api/logout", (req, res) => {
     req.logout();
-    res.send(req.user);
+    res.redirect("/");
   });
 
   app.get("/api/current_user", (req, res) => {
     res.send(req.user);
   });
 
-  app.get("/", (req, res) => {
+  app.get("/", res => {
     res.json({ hi: "hello buddy" });
   });
 };
